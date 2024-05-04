2995314e4a8231d05da81e02118510ba5723c17e
nester2369 <160327082+nester2369@users.noreply.github.com>
2024-05-04 07:40:55 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,16 @@
+def find_mm(str):
+    bfint = 0
+    bfstr = ''
+    for i in range(1, len(str)):
+        for j in range(0, i):
+            if (str[j:i]) >= bfint and len(bfstr) < len(str[j:i])):
+                bfint = str.count(str[j:i])
+            bfstr = str[j:i]
+            elif (str.count(str[j:i]) > bfint):
+            bfint = str.count(str[j:i])
+            bfstr = str[j:i]
+    return bfint, bfstr
+
+
+st = input()
+print(find_mm(st))
---
+++ 3.py
@@ -0,0 +1,19 @@
+def count_routes(n, m):
+    routes = [[0] * m for _ in range(n)]
+
+
+    routes[0][0] = 1
+
+
+    for i in range(n):
+        for j in range(m):
+            if i != 0 or j != 0:
+                routes[i][j] = routes[i - 1][j] + routes[i][j - 1]
+
+    return routes[n - 1][m - 1]
+
+
+n = int(input("number of lines: "))
+m = int(input("number of columns: "))
+
+print(f"number of routes from A to B: {count_routes(n, m)}")
